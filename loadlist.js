4e80add9c180660922ac31c378176c3e377e29d7
tt52551 <tt525519836@gmail.com>
2022-02-18 13:47:39 +1000

    adding some links

--- loadlist.js
+++ loadlist.js
@@ -87,6 +87,24 @@ function gen_card_template(tableId, category) {
     return template;
 };
 
+function initCards() {
+    var i = 0;
+    var col_1 = document.getElementById("col_1");
+    var col_2 = document.getElementById("col_2");
+    for (var [key, value] of listMap) {
+        var card = document.createElement("div");
+        card.innerHTML = gen_card_template(key, value[1]);
+        if (i % 2 == 0){
+            col_1.appendChild(card);
+        } else {
+            col_2.appendChild(card);
+        }
+        showSome(key);
+
+        i = i + 1;
+    }
+}
+
 var hot_list = new Array({
     img: "icons/搜狗浏览器.jpg",
     name: "搜狗浏览器",
@@ -129,7 +147,13 @@ var hot_list = new Array({
     href: "https://www.youdao.com",
     language: "中文",
     chargeType: "基本版免费"
-}, );
+},  {
+    img: "icons/sou-gou-pinyin.png",
+    name: "搜狗拼音输入法",
+    href: "https://pinyin.sogou.com",
+    language: "中文",
+    chargeType: "完全免费"
+},);
 
 var pdf_list = new Array({
     img: "icons/adobe_reader.png",
@@ -169,8 +193,19 @@ var videoPlayer_list = new Array({
 
 var photoEditor_list = new Array(
     {
-
-    }, 
+        img: "icons/gimp.png",
+        name: "GIMP",
+        href: "https://www.gimp.org",
+        language: "英文",
+        chargeType: "完全免费"
+    },
+    {
+        img: "icons/hitfilm-express.png",
+        name: "HitFilm Express",
+        href: "https://fxhome.com/product/hitfilm-express",
+        language: "英文",
+        chargeType: "完全免费"
+    },
 );
 
 var audioEditor_list = new Array({
@@ -181,25 +216,128 @@ var audioEditor_list = new Array({
     chargeType: "完全免费"
 }, );
 
-var videoEditor_list = new Array({
-
+var videoEditor_list = new Array(    {
+    img: "icons/hitfilm-express.png",
+    name: "HitFilm Express",
+    href: "https://fxhome.com/product/hitfilm-express",
+    language: "英文",
+    chargeType: "完全免费"
 }, );
+
+var devTool_list = new Array({
+    img: "icons/scoop.jpg",
+    name: "Scoop",
+    href: "https://scoop.sh",
+    language: "英文",
+    chargeType: "完全免费"
+}, {
+    img: "icons/chocolatey.png",
+    name: "Chocolatey",
+    href: "https://chocolatey.org",
+    language: "英文",
+    chargeType: "完全免费"
+}, {
+    img: "icons/putty.png",
+    name: "PuTTY",
+    href: "https://www.putty.org",
+    language: "英文",
+    chargeType: "完全免费"
+}, {
+    img: "icons/winscp.png",
+    name: "WinSCP",
+    href: "https://winscp.net/eng/index.php",
+    language: "英文",
+    chargeType: "完全免费"
+},{
+    img: "icons/greenshot.png",
+    name: "Greenshot",
+    href: "https://getgreenshot.org",
+    language: "英文",
+    chargeType: "完全免费"
+},{
+    img: "icons/colora-color-picker.png",
+    name: "Colora - Color Picker",
+    href: "https://bluegrams.github.io/Colora",
+    language: "英文",
+    chargeType: "完全免费"
+},{
+    img: "icons/curl.png",
+    name: "Curl",
+    href: "https://curl.se",
+    language: "英文",
+    chargeType: "完全免费"
+},{
+    img: "icons/sourcetree.png",
+    name: "Sourcetree",
+    href: "https://www.sourcetreeapp.com",
+    language: "英文",
+    chargeType: "完全免费"
+},); // dev
 
 var remote_list = new Array({
     img: "icons/anydesk.png",
     name: "AnyDesk",
     href: "https://anydesk.com/zhs",
     language: "多国语言",
-    chargeType: "个人免费"
-}, );
+    chargeType: "个人版免费"
+}, ); // remote
 
-var template_list = new Array({
-    img: "icons/",
-    name: "",
-    href: "",
-    language: "",
-    chargeType: ""
-}, );
+var textEditor_list = new Array(
+    {
+        img: "icons/notepad-plug-plug.png",
+        name: "NotePad++",
+        href: "https://notepad-plus-plus.org",
+        language: "多国语言",
+        chargeType: "完全免费"
+    },
+    {
+        img: "icons/markdownpad.png",
+        name: "MarkdownPad",
+        href: "https://markdownpad.com",
+        language: "英文",
+        chargeType: "基本版免费"
+    },
+); // text editor
+
+var decompiler_list = new Array(
+    {
+        img: "icons/java-decompiler.jpg",
+        name: "Java Decompiler",
+        href: "https://java-decompiler.github.io",
+        language: "英文",
+        chargeType: "完全免费"
+    },
+); // decompiler
+
+var other_list = new Array(
+    {
+        img: "icons/fast-copy.png",
+        name: "Fast Copy",
+        href: "https://fastcopy.jp",
+        language: "英文",
+        chargeType: "完全免费"
+    },
+); // other
+
+var downloadTool_list = new Array(
+    {
+        img: "icons/4k-video-downloader.png",
+        name: "4K Video Downloader",
+        href: "https://www.4kdownload.com",
+        language: "英文",
+        chargeType: "基本版免费"
+    },
+); // download tool
+
+var template_list = new Array(
+    {
+        img: "icons/",
+        name: "",
+        href: "",
+        language: "",
+        chargeType: ""
+    },
+);
 
 var listMap = new Map([
     ["hot_tbody", [hot_list, "常用"]],
@@ -210,26 +348,13 @@ var listMap = new Map([
     ["audioEditor_tbody", [audioEditor_list, "音频编辑"]],
     ["videoEditor_tbody", [videoEditor_list, "视频编辑"]],
     ["remote_tbody", [remote_list, "远程桌面"]],
-
-
+    ["devTool_tbody", [devTool_list, "开发人员常用工具"]],
+    ["textEditor_tbody", [textEditor_list, "文本编辑器"]],
+    ["decompiler_tbody", [decompiler_list, "反编译工具"]],
+    ["downloadTool_tbody", [downloadTool_list, "下载工具"]],
+    
+    
+    ["other_tbody", [other_list, "其它"]],
 ]);
-
-function initCards() {
-    var i = 0;
-    var col_1 = document.getElementById("col_1");
-    var col_2 = document.getElementById("col_2");
-    for (var [key, value] of listMap) {
-        var card = document.createElement("div");
-        card.innerHTML = gen_card_template(key, value[1]);
-        if (i % 2 == 0){
-            col_1.appendChild(card);
-        } else {
-            col_2.appendChild(card);
-        }
-        showSome(key);
-
-        i = i + 1;
-    }
-}
 
 initCards();
